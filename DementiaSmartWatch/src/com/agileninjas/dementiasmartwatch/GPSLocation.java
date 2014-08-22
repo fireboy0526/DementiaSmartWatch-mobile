eb5118fc25445f5e45bc9ff933f466bbeee87648
fireboy0526 <fireboy0526@gmail.com>
2014-08-22 15:22:30 +0800

    The GPSLocation Class gets the gps. Can be call in different class.

---
+++ DementiaSmartWatch/src/com/agileninjas/dementiasmartwatch/GPSLocation.java
@@ -0,0 +1,81 @@
+package com.agileninjas.dementiasmartwatch;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class GPSLocation implements LocationListener {
+	private static LocationManager locationManager;
+	private static String provider;
+	private static Criteria criteria;
+	private double gLatitude, gLongitude;
+
+	public static void runGPS(Context context) {
+		GPSLocation gps = new GPSLocation();
+		gps.start(context);
+	}
+	
+	public void start(Context context) {
+		 //Getting LocationManager Object
+        locationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+        
+        //Creating an empty criteria object
+        criteria = new Criteria();
+        
+        //Getting the name of the provider that meets the criteria
+        provider = locationManager.getBestProvider(criteria, false);
+        
+        if(provider != null && !provider.equals("")) {
+        	//Get the location from the given provider
+        	Location location = locationManager.getLastKnownLocation(provider);
+        	
+        	locationManager.requestLocationUpdates(provider, 1000, 1, this);
+        	
+        	if(location != null) {
+        		onLocationChanged(location);
+        	}
+        } else {
+        	Toast.makeText(context, "No Provider Found", Toast.LENGTH_SHORT).show();
+        }
+	}
+	
+	//To return Longitude
+	public double getLongitude() {
+		return gLongitude;
+	}
+	
+	//To return Latitude
+	public double getLatitude() {
+		return gLatitude;
+	}
+
+	//Run this when location changes
+	public void onLocationChanged(Location location) {
+		gLongitude = (double)(location.getLongitude());
+		gLatitude = (double)(location.getLatitude());
+		System.out.println("Lon: " + gLongitude + ", Lan: " + gLatitude);
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+}
