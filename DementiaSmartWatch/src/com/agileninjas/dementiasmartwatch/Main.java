08ac2f0e351f359bba0e661f3c048b644424f4cb
fireboy0526 <fireboy0526@gmail.com>
2014-08-24 18:34:06 +0800

    Revert "Changes made by johnny merged"
    This reverts commit a6a1704ac2a0c6c7524e1bae79f9b82655b71a08.

--- DementiaSmartWatch/src/com/agileninjas/dementiasmartwatch/Main.java
+++ DementiaSmartWatch/src/com/agileninjas/dementiasmartwatch/Main.java
@@ -119,7 +119,7 @@ public class Main extends Activity {
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
 		// while interacting with the UI.
-		findViewById(R.id.fullscreen_content_controls).setOnTouchListener(
+		findViewById(R.id.dummy_button).setOnTouchListener(
 				mDelayHideTouchListener);
 	}
 
