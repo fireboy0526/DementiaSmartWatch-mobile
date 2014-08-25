d2c388617fc0b305824918c738548ba6cb74c1b5
Mana Davarpanah <mana.davarpanah@yahoo.co.uk>
2014-08-25 11:45:47 +1000

    Unit Test Classes
    Main & GPSLocation

--- DementiaSmartWatch/src/com/agileninjas/dementiasmartwatch/tests/GPSLocationTest.java
+++ DementiaSmartWatch/src/com/agileninjas/dementiasmartwatch/tests/GPSLocationTest.java
@@ -1,36 +1,31 @@
 package com.agileninjas.dementiasmartwatch.tests;
 
+//import com.agileninjas.dementiasmartwatch.GPSLocation;
+
 import android.location.Location;
 //import android.location.LocationManager;
-import org.junit.Test;
+import android.test.AndroidTestCase;
 
-import com.agileninjas.dementiasmartwatch.GPSLocation;
 
-import static org.junit.Assert.*;
+public class GPSLocationTest extends AndroidTestCase{
 
-public class GPSLocationTest{
-	
 	private static final String PROVIDER = "My Provider";
     private static final double LAT = 37.377166;
     private static final double LNG = -122.086966;
     private Location mLocation = new Location(PROVIDER);
-    private static final double EPSILON = 10e-07;
-    private final GPSLocation testGPS = new GPSLocation();
+    //private final GPSLocation testGPS = new GPSLocation();
     //private final LocationManager mLocationManager;
 
-    @Test
 	public void getLongitudeTest() {
 		mLocation.setLongitude(LNG);
-		assertEquals(mLocation.getLongitude(), LNG, EPSILON);
+		assertEquals(mLocation.getLongitude(), LNG);
 	}
 	
-	@Test
 	public void getLatitudeTest() {
 		mLocation.setLatitude(LAT);
-		assertEquals(mLocation.getLatitude(), LAT, EPSILON);
+		assertEquals(mLocation.getLatitude(), LAT);
 	}
 	
-//	@Test
 //	public void onLocationChangedTest() {
 //		testGPS.onLocationChanged(mLocation);
 //		assertFalse(testGPS.getChanged());
