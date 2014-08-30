1ed59fcab0b62417de9eee3d454a99a48880a65c
fireboy0526 <fireboy0526@gmail.com>
2014-08-31 02:14:52 +1000

    Battery Check updated. Now sends email to relatives. Currently the relative email is agileninjas2014@gmail.com

--- DementiaSmartWatch/src/com/agileninjas/dementiasmartwatch/BatteryCheck.java
+++ DementiaSmartWatch/src/com/agileninjas/dementiasmartwatch/BatteryCheck.java
@@ -1,5 +1,20 @@
 package com.agileninjas.dementiasmartwatch;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,6 +25,7 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.BatteryManager;
+import android.util.Log;
 
 public class BatteryCheck {
 	private boolean batteryLow = false;
@@ -42,6 +58,48 @@ public class BatteryCheck {
 				} else if(level <= 20 && batteryLow == false){
 				    batteryLow = true;
 				    //Log.e("BraodcastNotifier", "GOT LOW BATTERY WARNING");
+				    
+				    //Send data
+					final HttpClient httpclient = new DefaultHttpClient();
+					final HttpPost httppost = new HttpPost("http://hungpohuang.com/agile/include/mail.php");
+					
+					//set data
+					String to = "agileninjas2014@gmail.com";
+					String subject = "Patient watch low battery";
+					String message = "Your patient battery is low, please recharged as soon as possible";
+					
+					//Adding data
+					List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+					nameValuePairs.add(new BasicNameValuePair("to", to));
+					nameValuePairs.add(new BasicNameValuePair("subject", subject));
+					nameValuePairs.add(new BasicNameValuePair("message", message));
+
+					//Encoding data
+					try {
+						httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+					} catch (UnsupportedEncodingException e) {
+			            // log exception
+			            e.printStackTrace();
+			        }
+					
+					//making Post Request
+					Runnable runnable = new Runnable() {
+						@Override
+						public void run() {
+							try {
+								ResponseHandler<String> responseHandler=new BasicResponseHandler();
+						        String responseBody = httpclient.execute(httppost, responseHandler);
+								Log.d("Response of POST: ", responseBody);
+							} catch (ClientProtocolException e) {
+							    e.printStackTrace();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+						
+					};
+					new Thread(runnable).start();
+				    
 				    //Plays default notification sound
 				    try {
 				    	Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
