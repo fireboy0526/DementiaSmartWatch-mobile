c451320d543dd66993df9b2b7de0d474916a72ba
fireboy0526 <fireboy0526@gmail.com>
2014-09-05 21:50:19 +1000

    GPS Distance Calculation Beta Version

--- DementiaSmartWatch/src/com/agileninjas/dementiasmartwatch/GPSLocation.java
+++ DementiaSmartWatch/src/com/agileninjas/dementiasmartwatch/GPSLocation.java
@@ -7,22 +7,31 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.params.ConnManagerParams;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 
+import android.app.IntentService;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -32,6 +41,7 @@ public class GPSLocation implements LocationListener {
 	private static Criteria criteria;
 	private double gLatitude, gLongitude, oldLon, oldLat;
 	private boolean locationChanged;
+	private List<NameValuePair> nameValuePairs;
 	
 	public static void runGPS(final Context context) {
 		final GPSLocation gps = new GPSLocation();
@@ -106,13 +116,15 @@ public class GPSLocation implements LocationListener {
 		}
 		
 		if (locationChanged == true) {
+			new asyncTask().execute();
+			/*
 			//Get Date timestamp
 			SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
 			String date = s.format(new Date());
 
 			//Send data
-			final HttpClient httpclient = new DefaultHttpClient();
-			final HttpPost httppost = new HttpPost("http://hungpohuang.com/agile/include/gpsrecord.php");
+			//httpclient = new DefaultHttpClient();
+			//httppost = new HttpPost("http://hungpohuang.com/agile/include/gpsrecord.php");
 			
 			//Adding data
 			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(4);
@@ -120,21 +132,21 @@ public class GPSLocation implements LocationListener {
 			nameValuePairs.add(new BasicNameValuePair("lat", String.valueOf(gLatitude)));
 			nameValuePairs.add(new BasicNameValuePair("lon", String.valueOf(gLongitude)));
 			nameValuePairs.add(new BasicNameValuePair("date", String.valueOf(date)));
-
+			*/
 			//Encoding data
-			try {
+			/*try {
 				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 			} catch (UnsupportedEncodingException e) {
 	            // log exception
 	            e.printStackTrace();
-	        }
+	        }*/
 			
-			//making Post Request
-			Runnable runnable = new Runnable() {
+			
+			/*Runnable runnable = new Runnable() {
 				@Override
 				public void run() {
 					try {
-						ResponseHandler<String> responseHandler=new BasicResponseHandler();
+						ResponseHandler<String> responseHandler = new BasicResponseHandler();
 				        String responseBody = httpclient.execute(httppost, responseHandler);
 						Log.d("Response of POST: ", responseBody);
 					} catch (ClientProtocolException e) {
@@ -145,7 +157,7 @@ public class GPSLocation implements LocationListener {
 				}
 				
 			};
-			new Thread(runnable).start();
+			new Thread(runnable).start();*/
 		}		
 	}
 
@@ -162,5 +174,59 @@ public class GPSLocation implements LocationListener {
 	@Override
 	public void onProviderDisabled(String provider) {
 		// TODO Auto-generated method stub
+	}
+
+	public class asyncTask extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				//Get Date timestamp
+				SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
+				String date = s.format(new Date());
+				
+				//Set Connection data
+				//HttpParams httpparams = new BasicHttpParams();
+				//ConnManagerParams.setTimeout(httpparams, 50000);
+				//HttpConnectionParams.setConnectionTimeout(httpparams, 50000);
+				//HttpConnectionParams.setSoTimeout(httpparams, 50000);
+				final HttpClient httpclient = new DefaultHttpClient();//httpparams);
+				final HttpPost httppost = new HttpPost("http://hungpohuang.com/agile/include/gpsrecord.php");
+				
+				//Adding data
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(4);
+				nameValuePairs.add(new BasicNameValuePair("user_id", UniqueID.getUniqueID()));
+				nameValuePairs.add(new BasicNameValuePair("lat", String.valueOf(gLatitude)));
+				nameValuePairs.add(new BasicNameValuePair("lon", String.valueOf(gLongitude)));
+				nameValuePairs.add(new BasicNameValuePair("date", String.valueOf(date)));
+				
+				//Encoding data
+				try {
+					httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+				} catch (UnsupportedEncodingException e) {
+		            // log exception
+		            e.printStackTrace();
+		        }
+				
+				Log.d("Response of http: ", httppost.toString());
+				//System.out.println("lon:" + oldLon);
+				//System.out.println("lat:" + oldLat);
+				
+				try {
+					//HttpResponse response = httpclient.execute(httppost);
+					ResponseHandler<String> responseHandler = new BasicResponseHandler();
+			        String responseBody = httpclient.execute(httppost, responseHandler);
+					Log.e("Response of POST: ", responseBody);
+				} catch (ClientProtocolException e) {
+				    e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
 	}
 }
