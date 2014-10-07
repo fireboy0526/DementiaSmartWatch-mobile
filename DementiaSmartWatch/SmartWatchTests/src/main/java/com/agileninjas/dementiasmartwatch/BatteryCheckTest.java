23dcb5a95af6aed5e57b912e452cd1a8b338479c
Mana Davarpanah <mana.davarpanah@yahoo.co.uk>
2014-10-07 11:10:20 +1000

    Maven project edited
    maven DementiaSmartWatch Parent
    maven DementiaSmartWatch Application
    maven DementiaSmartWatch Tests

---
+++ DementiaSmartWatch/SmartWatchTests/src/main/java/com/agileninjas/dementiasmartwatch/BatteryCheckTest.java
@@ -0,0 +1,55 @@
+package com.agileninjas.dementiasmartwatch;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+
+import java.io.*;  
+
+import com.agileninjas.dementiasmartwatch.BatteryCheck;
+import com.agileninjas.dementiasmartwatch.R;
+
+public class BatteryCheckTest extends AndroidTestCase{
+	
+	private BatteryCheck mBatteryCheck;
+	
+	@Override
+    protected void setUp() throws Exception {
+		
+		super.setUp();
+		
+		mBatteryCheck = new BatteryCheck();		
+	}
+	
+//	private void openScript() throws IOException
+//	{
+//		String [] cmd = {"open", "/Users/mana/Desktop/sc.command"};
+//		Runtime.getRuntime().exec(cmd);
+//	}
+	
+	//set power capacity to 100
+	public void testBatteryLevelNormalCases1() {
+		mBatteryCheck.getBatterLevel(getContext());
+		assertFalse(mBatteryCheck.getBatteryLow());
+	}
+	
+	//set power capacity to 10
+	public void testBatteryLevelNormalCases2() throws IOException{
+		//script.openScript();
+		mBatteryCheck.getBatterLevel(getContext());
+		assertTrue(mBatteryCheck.getBatteryLow());
+	}
+	
+	//set power capacity to 20
+	public void testBatteryLevelBoundaryCases() {
+		mBatteryCheck.getBatterLevel(getContext());
+		assertTrue(mBatteryCheck.getBatteryLow());
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	
+}
